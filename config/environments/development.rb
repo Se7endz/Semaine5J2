f0a5f24b9e0cf91fbd5370c07443b2eb9563a783
Se7endz <oussama.bendifallahd@gmail.com>
2019-05-07 16:41:57 +0200

    test2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,14 +5,14 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.action_mailer.perform_deliveries = true
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  
+
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
